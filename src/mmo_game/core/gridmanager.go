56b80171afc45de6041cbbe0f6d7a2fc09230536
kunming xie <xie.kunming@astute-tec.com>
2020-12-10 20:40:41 +0800

    实现AOI管理

--- src/mmo_game/core/gridmanager.go
+++ src/mmo_game/core/gridmanager.go
@@ -66,3 +66,89 @@ func (m *GridManager) String() string {
 
 	return s
 }
+
+// GetSurroundGrids 根据GID获取周围的所有格子
+func (m *GridManager) GetSurroundGrids(gid int) (grids []*Grid) {
+	if _, success := m.Grids[gid]; !success {
+		return
+	}
+
+	grids = append(grids, m.Grids[gid])
+
+	idx := gid % m.X
+
+	if idx > 0 {
+		grids = append(grids, m.Grids[gid-1])
+	}
+
+	if idx < m.X-1 {
+		grids = append(grids, m.Grids[gid+1])
+	}
+
+	gidsX := make([]int, 0, len(grids))
+	for _, v := range grids {
+		gidsX = append(gidsX, v.ID)
+	}
+
+	for _, v := range gidsX {
+		idy := v / m.Y
+
+		if idy > 0 {
+			grids = append(grids, m.Grids[v-m.X])
+		}
+
+		if idy < m.Y-1 {
+			grids = append(grids, m.Grids[v+m.X])
+		}
+	}
+
+	return grids
+}
+
+// GetGidByPosition 通过x,y坐标得到当前的GID格子编号
+func (m *GridManager) GetGidByPosition(x, y float32) int {
+	idx := (int(x) - m.MinX) / m.GridWidth()
+	idy := (int(y) - m.MinY) / m.GridHeight()
+
+	return idy*m.X + idx
+}
+
+// GetAllPlayerIDByPosition 通过x, y坐标得到周边九宫格内全部的playerids
+func (m *GridManager) GetAllPlayerIDByPosition(x, y float32) (players []int) {
+	gid := m.GetGidByPosition(x, y)
+
+	grids := m.GetSurroundGrids(gid)
+
+	for _, v := range grids {
+		v.GetPlayerIDs(players)
+	}
+
+	return players
+}
+
+// AddPlayerIDToGrid 添加一个playerid到格子
+func (m *GridManager) AddPlayerIDToGrid(pid, gid int) {
+	m.Grids[gid].Add(pid)
+}
+
+// RemovePlayerIDFromGrid 删除一个格子内的playerid
+func (m *GridManager) RemovePlayerIDFromGrid(pid, gid int) {
+	m.Grids[gid].Remove(pid)
+}
+
+// GetAllPlayerIDByGid 通过gid获取全部的playerid
+func (m *GridManager) GetAllPlayerIDByGid(gid int) (players []int) {
+	m.Grids[gid].GetPlayerIDs(players)
+
+	return players
+}
+
+// AddPositionToGrid 通过坐标讲player添加到格子
+func (m *GridManager) AddPositionToGrid(x, y float32, gid int) {
+	m.Grids[gid].Add(m.GetGidByPosition(x, y))
+}
+
+// RemovePositionFromGrid 通过坐标从格子删除player
+func (m *GridManager) RemovePositionFromGrid(x, y float32, gid int) {
+	m.Grids[gid].Remove(m.GetGidByPosition(x, y))
+}
--- src/mmo_game/core/gridmanager_test.go
+++ src/mmo_game/core/gridmanager_test.go
@@ -9,3 +9,19 @@ func TestNewGridManager(t *testing.T) {
 	mgr := NewGridManager(0, 250, 5, 0, 250, 5)
 	fmt.Println(mgr.String())
 }
+
+func TestGetSurroundGrids(t *testing.T) {
+	mgr := NewGridManager(0, 250, 5, 0, 250, 5)
+
+	for gid := range mgr.Grids {
+		grids := mgr.GetSurroundGrids(gid)
+		fmt.Println("gid: ", gid, "grids len: ", len(grids))
+
+		gids := make([]int, 0, len(grids))
+		for _, grid := range grids {
+			gids = append(gids, grid.ID)
+		}
+
+		fmt.Println(" surrounding grids ", gids)
+	}
+}
